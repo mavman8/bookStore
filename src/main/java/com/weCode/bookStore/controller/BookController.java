e62f14a676fd2fd6b95a43465ebd5fa7c4f18f13
Tang <stephen.tang@accenture.com>
2022-11-29 17:42:29 -0500

    Initiation of project

---
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -0,0 +1,30 @@
+package com.weCode.bookStore.controller;
+
+import com.weCode.bookStore.dto.BookDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> getBooks(){
+
+        BookDTO book = BookDTO.builder()
+                        .title("My first book title")
+                        .build();
+
+        List<BookDTO> books = new ArrayList<>();
+        books.add(book);
+        return ResponseEntity.ok(books);
+    }
+
+
+
+}
---
+++ src/main/java/com/weCode/bookStore/dto/BookDTO.java
@@ -0,0 +1,18 @@
+package com.weCode.bookStore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDTO {
+
+    private String title;
+
+
+}
